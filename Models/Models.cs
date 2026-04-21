42ca66e27dd4a10e6ad06608b4d34f206f3e1911
jutercek <jutercek@gmail.com>
2026-04-21 13:12:28 +0200

    Initial commit

---
+++ Models/Models.cs
@@ -0,0 +1,58 @@
+﻿namespace MediaRake.Models;
+
+public enum FieldType
+{
+    Score,
+    Text,
+    Image
+}
+
+public class FieldDefinition // This defines a Name-Type combo for defining fields
+{
+    public string Name { get; set; } = "";
+    public FieldType Type { get; set; } = FieldType.Score;
+}
+
+public class FieldValue
+{
+    public int? Score { get; set; }
+    public string? Text { get; set; }
+}
+
+public class MediaItem
+{
+    public Guid Id { get; set; } = Guid.NewGuid(); // This is a unique ID identifier
+    public string Title { get; set; } = "";
+    public Dictionary<string, FieldValue> Values { get; set; } = new();
+    public List<string> Tags { get; set; } = new();
+}
+
+public class RatingList
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string Name { get; set; } = "";
+    public List<MediaItem> Items { get; set; } = new();
+    public List<FieldDefinition> Fields { get; set; } = new();
+}
+
+public class Subcategory
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string Name { get; set; } = "";
+    public List<RatingList> Lists { get; set; } = new();
+
+}
+
+public class Category
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string Name { get; set; } = "";
+    public List<RatingList> Lists { get; set; } = new();
+    public List<Subcategory> Subcategories { get; set; } = new();
+
+}
+
+public class AppData
+{
+    public List<Category> Categories { get; set; } = new();
+}
